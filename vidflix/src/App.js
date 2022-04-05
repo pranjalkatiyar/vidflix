8ad1c0ad5b09b3ab27df2913508d7ab4774578d1
vranda25 <202052349@iiitvadodara.ac.in>
2022-04-05 11:18:38 +0530

    use content

--- vidflix/src/App.js
+++ vidflix/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import {JumbotronContainer} from './containers/jumbotron'
+import { useAuthListener } from './hooks';
+
+export default function App(){
+  const{user} = useAuthListner();
+  console.log(user); 
+}
 
 const App = () => {
   return (
   <JumbotronContainer/>
-  
+
   )
 }
 
 
-export default App;
+//export default App; 
---
+++ vidflix/src/hooks/index.js
@@ -0,0 +1,2 @@
+export {defalut as useContent} from './use-content';
+export {defalut as useAuthListener} from './use-auth-listener';
