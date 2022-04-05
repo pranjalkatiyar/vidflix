0d2fae086a1f09ad990dee36a648fcb80123d4e4
HarshiniPR <harshiprana@gmail.com>
2022-04-05 12:39:17 +0530

    firebase changes

--- vidflix/src/index.js
+++ vidflix/src/index.js
@@ -2,12 +2,17 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import {GlobalStyles} from './globalStyles';
 import 'normalize.css';
+import { firebase } from './lib/firebase.prod';
+import {FirebaseContext} from './context/firebase';
 
 //JSx fragment is used
 
-ReactDOM.render(<>
+ReactDOM.render(
+<>
+<FirebaseContext.Provider value={{firebase}}>
 <GlobalStyles/>
 <App/>
+</FirebaseContext.Provider>
 </>,
     document.getElementById('root')
 );
